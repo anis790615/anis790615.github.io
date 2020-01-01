009cefac08b365e86ff494b4f53eba75bb9f9175
Anis Alkomem <53934963+anis790615@users.noreply.github.com>
2020-01-01 21:43:57 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
+// Intialize and add the map
+function initMap() {
+  // Your location
+  const loc = {lat: 51.930632, lng: 4.497466};
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 17,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
 
+// Sticky menu transparent background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
+// Smooth Scrolling
+$('#navbar a').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+
+      },
+      800
+    );
+  }
+});
